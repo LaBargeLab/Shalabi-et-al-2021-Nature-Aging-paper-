f3caa444794620520558ee42cc8c73d1c4a7a172
Sundus Shalabi <76931466+sshalabi@users.noreply.github.com>
2021-07-15 17:21:38 -0700

    Create cKit_differentiation_analysis.r

---
+++ cKit_differentiation_analysis.r
@@ -0,0 +1,5 @@
+
+### This file provides the R code for producing the results in the cKit progenitor differentiation assays.
+
+### Load file of the quantified mean KRT19 and KRT14 signals from the fixed and stained cKit progenitor cells at 2 or 7 days. 
+### This file is supplementary table 
